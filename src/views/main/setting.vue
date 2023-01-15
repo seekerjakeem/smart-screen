4fe606abbc4ba166f2d7e404dabd276937cd4835
seekerjakeem <jakeem@yeah.net>
2023-01-15 15:29:02 +0800

    修改kafka config

--- src/views/main/setting.vue
+++ src/views/main/setting.vue
@@ -8,48 +8,44 @@
         <text style="color: white;font-weight: 900;font-size: 34px;margin-left: 100px;margin-top: 50px">串口设置</text>
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600">端口号</text>
-          <el-input v-model="dPort" class="w-50 m-2" placeholder="请输入端口号" style="width: 130px;margin-left: 20px"/>
+          <el-input v-model="dPort" class="w-50 m-2" placeholder="请输入端口号" style="width: 130px;margin-left: 20px" />
 
           <text style="font-size: 22px;color: white;font-weight: 600;margin-left: 80px">数据位</text>
           <el-input v-model="dataPosition" class="w-50 m-2" placeholder="请输入数据位"
-                    style="width: 130px;margin-left: 20px"/>
+            style="width: 130px;margin-left: 20px" />
         </div>
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600">波特率</text>
-          <el-input v-model="btl" class="w-50 m-2" placeholder="请输入波特率" style="width: 130px;margin-left: 20px"/>
+          <el-input v-model="btl" class="w-50 m-2" placeholder="请输入波特率" style="width: 130px;margin-left: 20px" />
 
           <text style="font-size: 22px;color: white;font-weight: 600;margin-left: 80px">停止位</text>
-          <el-input v-model="txw" class="w-50 m-2" placeholder="请输入停止位" style="width: 130px;margin-left: 20px"/>
+          <el-input v-model="txw" class="w-50 m-2" placeholder="请输入停止位" style="width: 130px;margin-left: 20px" />
         </div>
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600">校验位</text>
-          <el-input v-model="jyw" class="w-50 m-2" placeholder="请输入校验位" style="width: 130px;margin-left: 20px"/>
+          <el-input v-model="jyw" class="w-50 m-2" placeholder="请输入校验位" style="width: 130px;margin-left: 20px" />
         </div>
       </div>
       <div class="top-right">
         <text style="color: white;font-weight: 900;font-size: 34px;margin-left: 330px">转发串口设置</text>
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600;margin-left: 180px">端口号</text>
-          <el-input v-model="zfckPort" class="w-50 m-2" placeholder="请输入端口号"
-                    style="width: 130px;margin-left: 25px"/>
+          <el-input v-model="zfckPort" class="w-50 m-2" placeholder="请输入端口号" style="width: 130px;margin-left: 25px" />
         </div>
 
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600;margin-left: 230px">波特率</text>
-          <el-input v-model="zfckBtl" class="w-50 m-2" placeholder="请输入波特率"
-                    style="width: 133px;margin-left: 30px"/>
+          <el-input v-model="zfckBtl" class="w-50 m-2" placeholder="请输入波特率" style="width: 133px;margin-left: 30px" />
         </div>
 
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600;margin-left: 260px">校验位</text>
-          <el-input v-model="zfckXyw" class="w-50 m-2" placeholder="请输入校验位"
-                    style="width: 130px;margin-left: 25px"/>
+          <el-input v-model="zfckXyw" class="w-50 m-2" placeholder="请输入校验位" style="width: 130px;margin-left: 25px" />
         </div>
 
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600;margin-left: 280px">数据位</text>
-          <el-input v-model="zfckData" class="w-50 m-2" placeholder="请输入数据位"
-                    style="width: 133px;margin-left: 30px"/>
+          <el-input v-model="zfckData" class="w-50 m-2" placeholder="请输入数据位" style="width: 133px;margin-left: 30px" />
         </div>
       </div>
     </div>
@@ -57,14 +53,12 @@
       <div class="bottom-left">
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600;margin-left: 70px">服务器IP</text>
-          <el-input v-model="fwIp" class="w-50 m-2" placeholder="请输入服务器IP"
-                    style="width: 130px;margin-left: 25px"/>
+          <el-input v-model="fwIp" class="w-50 m-2" placeholder="请输入服务器IP" style="width: 130px;margin-left: 25px" />
         </div>
 
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600;margin-left: 150px">服务器Port</text>
-          <el-input v-model="fwPort" class="w-50 m-2" placeholder="请输入服务器Port"
-                    style="width: 133px;margin-left: 30px"/>
+          <el-input v-model="fwPort" class="w-50 m-2" placeholder="请输入服务器Port" style="width: 133px;margin-left: 30px" />
         </div>
         <text style="color: white;font-weight: 900;font-size: 34px;margin-left: 450px; margin-top: 103px">服务器设置
         </text>
@@ -72,41 +66,53 @@
       <div class="bottom-right">
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600;margin-left: 270px">Kafak IP</text>
-          <el-input v-model="kafakIP" class="w-50 m-2" placeholder="请输入Kafak IP"
-                    style="width: 130px;margin-left: 25px"/>
+          <el-input v-model="kafakIP" class="w-50 m-2" placeholder="请输入Kafak IP" v-on:change="kafakIpSettingChangeEvent"
+            style="width: 130px;margin-left: 25px" />
         </div>
 
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600;margin-left: 247px">Kafak User</text>
           <el-input v-model="kafakUser" class="w-50 m-2" placeholder="请输入Kafak User"
-                    style="width: 133px;margin-left: 10px"/>
+            v-on:change="kafakUserNameSettingChangeEvent" style="width: 133px;margin-left: 10px" />
         </div>
 
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600;margin-left: 220px">Kafak Port</text>
           <el-input v-model="kafakPort" class="w-50 m-2" placeholder="请输入Kafak Port"
-                    style="width: 130px;margin-left: 25px"/>
+            v-on:change="kafakPortSettingChangeEvent" style="width: 130px;margin-left: 25px" />
         </div>
 
         <div class="form-div">
           <text style="font-size: 22px;color: white;font-weight: 600;margin-left: 170px">Kafak Passwd</text>
-          <el-input v-model="kafakPasswd" class="w-50 m-2" placeholder="请输入Kafak Passwd"
-                    style="width: 155px;margin-left: 30px"/>
+          <el-input v-model="kafakPasswd" class="w-50 m-2" placeholder="请输入Kafak Passwd" show-password
+            v-on:change="kafakPassWdSettingChangeEvent" style="width: 155px;margin-left: 30px" />
         </div>
 
         <text style="color: white;font-weight: 900;font-size: 34px;margin-left: 630px;margin-top: -5px">Kafka设置</text>
 
         <img @click="sure" src="../setting-imgs/Sure.png"
-             style="width: 200px;height: 80px;margin-left: -100px;margin-top: 90px"/>
+          style="width: 200px;height: 80px;margin-left: -100px;margin-top: 90px; cursor:pointer" />
       </div>
     </div>
   </div>
-</template>
+</template> 
 
 
 <script setup>
 
-import {ref} from "vue";
+import { ref, onMounted} from "vue";
+import {kafka} from "../../assets/config.json";
+import socket from '../../socket/index'
+
+
+onMounted(() => {
+  kafakIP.value = kafka.ip;
+  kafakUser.value = kafka.username;
+  kafakPort.value = kafka.port;
+  kafakPasswd.value = kafka.password;
+  console.log('kafka',kafka);
+})
+
 
 const dPort = ref('')  //端口
 const dataPosition = ref('') //数据位
@@ -129,14 +135,82 @@ const kafakPort = ref('') //Kafak Port
 const kafakPasswd = ref('') //Kafak Passwd
 
 const sure = () => {
-  console.log('点击了确定按钮，保存了设置')
+  console.log('点击了确定按钮，保存了设置');
+  let config = {
+    ip: kafakIP.value,
+    port: kafakUser.value,
+    username: kafakUser.value,
+    password: kafakPasswd.value
+  }
+  let kafkaConfig = {
+    kafka: config
+  }
+
+  console.log('kafkaConfig', kafkaConfig);
+
+  socket.emit(JSON.stringify(kafkaConfig));
 }
+
+const kafakIpSettingChangeEvent = () => {
+  console.log('kafakIP', kafakIP.value);
+  if (!validateIp(kafakIP.value)) {
+    console.log('请输入有效的IP地址');
+    return;
+  }
+}
+
+const validateIp = (ip) => {
+  const regexIP = /^((25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))$/;
+  return regexIP.test(ip + '');
+}
+
+const kafakPortSettingChangeEvent = () => {
+  console.log('kafakPort', kafakPort.value);
+  if (!validatePort(kafakPort.value)) {
+    console.log("请输入有效端口号");
+    return;
+  }
+
+}
+
+const validatePort = (port) => {
+  const regexPort = /^[1-9]$|(^[1-9][0-9]$)|(^[1-9][0-9][0-9]$)|(^[1-9][0-9][0-9][0-9]$)|(^[1-6][0-5][0-5][0-3][0-5]$)/;
+  return regexPort.test(port + '');
+}
+
+const kafakUserNameSettingChangeEvent = () => {
+  console.log('kafakUser', kafakUser.value);
+  if (!usernameValid(kafakUser.value)) {
+    console.log('用户名 2-20位，只能包含汉字/数字/字母和下划线');
+    return;
+  }
+
+}
+//用户名正则，2到20位（字母，数字，下划线，减号）
+const usernameValid = (username) => {
+  const regexUserName = /^[a-zA-Z0-9_-]{2,20}$/;
+  return regexUserName.test(username + '');
+
+}
+
+const kafakPassWdSettingChangeEvent = () => {
+  console.log('kafakPasswd', kafakPasswd.value);
+  if (!passwdValid(kafakPasswd.value)) {
+    console.log('密码必须是纯数字，纯字母，纯特殊字符');
+    return;
+  }
+}
+const passwdValid = (passwd) => {
+  const regexPasswd = /^(?:\d+|[a-zA-Z]+|[!@#$%^&*]+)$/;
+  regexPasswd.test(passwd + '');
+}
+
+
 
 </script>
 
 
 <style lang="scss">
-
 body {
   margin: 0;
   padding: 0;
